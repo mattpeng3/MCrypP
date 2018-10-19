0552e590b253598853b1d2b322f27791fc6a02b8
Matt Peng <mattpeng3@gmail.com>
2018-10-19 00:22:56 -0700

    Compiles assets for Github Pages

---
+++ code/docs/js/app.js
@@ -0,0 +1,133 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+  loading: false,
+  tokenPrice: 1000000000000000,
+  tokensSold: 0,
+  tokensAvailable: 750000,
+
+  init: function() {
+    console.log("App initialized...")
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContracts();
+  },
+
+  initContracts: function() {
+    $.getJSON("MYPcrypSale.json", function(myp) {
+      App.contracts.MYPcrypSale = TruffleContract(myp);
+      App.contracts.MYPcrypSale.setProvider(App.web3Provider);
+      App.contracts.MYPcrypSale.deployed().then(function(myp) {
+        console.log("MYPcryp Sale Address:", myp.address);
+      });
+    }).done(function() {
+      $.getJSON("MYPcryp.json", function(mypcryp) {
+        App.contracts.MYPcryp = TruffleContract(mypcryp);
+        App.contracts.MYPcryp.setProvider(App.web3Provider);
+        App.contracts.MYPcryp.deployed().then(function(mypcryp) {
+          console.log("MYPcryp Address:", mypcryp.address);
+        });
+
+        App.listenForEvents();
+        return App.render();
+      });
+    })
+  },
+
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.MYPcrypSale.deployed().then(function(instance) {
+      instance.Sell({}, {
+        fromBlock: 0,
+        toBlock: 'latest',
+      }).watch(function(error, event) {
+        console.log("event triggered", event);
+        App.render();
+      })
+    })
+  },
+
+  render: function() {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    var loader  = $('#loader');
+    var content = $('#content');
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $('#accountAddress').html("Your Account: " + account);
+      }
+    })
+
+    // Load token sale contract
+    App.contracts.MYPcrypSale.deployed().then(function(instance) {
+      MypcrypSaleInstance = instance;
+      return MypcrypSaleInstance.tokenPrice();
+    }).then(function(tokenPrice) {
+      App.tokenPrice = tokenPrice;
+      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+      return MypcrypSaleInstance.tokensSold();
+    }).then(function(tokensSold) {
+      App.tokensSold = tokensSold.toNumber();
+      $('.tokens-sold').html(App.tokensSold);
+      $('.tokens-available').html(App.tokensAvailable);
+
+      var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+      $('#progress').css('width', progressPercent + '%');
+
+      // Load token contract
+      App.contracts.MYPcryp.deployed().then(function(instance) {
+        MYPcrypInstance = instance;
+        return MYPcrypInstance.balance(App.account);
+      }).then(function(balance) {
+        $('.MYP-balance').html(balance.toNumber());
+        App.loading = false;
+        loader.hide();
+        content.show();
+      })
+    });
+  },
+
+  buyTokens: function() {
+    $('#content').hide();
+    $('#loader').show();
+    var numberOfTokens = $('#numberOfTokens').val();
+    App.contracts.MYPcrypSale.deployed().then(function(instance) {
+      return instance.buyTokens(numberOfTokens, {
+        from: App.account,
+        value: numberOfTokens * App.tokenPrice,
+        gas: 500000 // Gas limit
+      });
+    }).then(function(result) {
+      console.log("Tokens bought...")
+      $('form').trigger('reset') // reset number of tokens in form
+      // Wait for Sell event
+    });
+  }
+}
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  })
+});
